37b63cb18099151d9de33845cf82fa4b5d0b045c
Madhusudanan Kandasamy <madhusudanan@in.ibm.com>
2016-07-28 15:44:00 +0530

    sample code

--- src/main/scala/org/apache/spark/sql/Utils.scala
+++ src/main/scala/org/apache/spark/sql/Utils.scala
@@ -3,191 +3,61 @@ package org.apache.spark.sql
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.codegen._
-import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeSet, BindReferences, Expression, IsNotNull, NullIntolerant, PredicateHelper, SortOrder, SubqueryExpression}
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeSet, BindReferences, Expression, GenericInternalRow, IsNotNull, NullIntolerant, PredicateHelper, SortOrder, SubqueryExpression}
 import org.apache.spark.sql.catalyst.planning.GenericStrategy
 import org.apache.spark.sql.catalyst.plans.logical.{Filter, LogicalPlan, UnaryNode}
 import org.apache.spark.sql.execution.metric.SQLMetrics
-import org.apache.spark.sql.execution.{CodegenSupport, FilterExec, SparkPlan, UnaryExecNode}
+import org.apache.spark.sql.execution._
+import org.apache.spark.sql.types.StructType
 
 /**
   * Created by madhusudanan on 26/07/16.
   */
 
-/** Physical plan for Filter. */
-case class FilterExec(condition: Expression, child: SparkPlan)
-  extends UnaryExecNode with CodegenSupport with PredicateHelper {
-
-  override def supportCodegen: Boolean = false
-
-  // Split out all the IsNotNulls from condition.
-  private val (notNullPreds, otherPreds) = splitConjunctivePredicates(condition).partition {
-    case IsNotNull(a: NullIntolerant) if a.references.subsetOf(child.outputSet) => true
-    case _ => false
-  }
-
-  // The columns that will filtered out by `IsNotNull` could be considered as not nullable.
-  private val notNullAttributes = notNullPreds.flatMap(_.references).distinct.map(_.exprId)
-
-  // Mark this as empty. We'll evaluate the input during doConsume(). We don't want to evaluate
-  // all the variables at the beginning to take advantage of short circuiting.
-  override def usedInputs: AttributeSet = AttributeSet.empty
+case class MAPGPUExec(inp: String, child: SparkPlan)
+  extends UnaryExecNode {
 
   override def output: Seq[Attribute] = {
-    child.output.map { a =>
-      if (a.nullable && notNullAttributes.contains(a.exprId)) {
-        a.withNullability(false)
-      } else {
-        a
-      }
-    }
+    child.output.map { a => a}
   }
 
   private[sql] override lazy val metrics = Map(
     "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"))
 
-  override def inputRDDs(): Seq[RDD[InternalRow]] = {
-    child.asInstanceOf[CodegenSupport].inputRDDs()
-  }
-
-  protected override def doProduce(ctx: CodegenContext): String = {
-    val source = child.asInstanceOf[CodegenSupport].produce(ctx, this)
-
-    val cleanedSource = CodeFormatter.stripOverlappingComments(
-      new CodeAndComment(CodeFormatter.stripExtraNewLines(source), ctx.getPlaceHolderToComments()))
-    println("Filter's doProduce " + CodeFormatter.format(cleanedSource))
-
-    source
-  }
-
-  override def doConsume(ctx: CodegenContext, input: Seq[ExprCode], row: ExprCode): String = {
-    val numOutput = metricTerm(ctx, "numOutputRows")
-
-    /**
-      * Generates code for `c`, using `in` for input attributes and `attrs` for nullability.
-      */
-    def genPredicate(c: Expression, in: Seq[ExprCode], attrs: Seq[Attribute]): String = {
-      val bound = BindReferences.bindReference(c, attrs)
-      val evaluated = evaluateRequiredVariables(child.output, in, c.references)
-
-      // Generate the code for the predicate.
-      val ev = ExpressionCanonicalizer.execute(bound).genCode(ctx)
-      val nullCheck = if (bound.nullable) {
-        s"${ev.isNull} || "
-      } else {
-        s""
-      }
-
-      s"""
-         |$evaluated
-         |${ev.code}
-         |if (${nullCheck}!${ev.value}) continue;
-       """.stripMargin
-    }
-
-    ctx.currentVars = input
-
-    // To generate the predicates we will follow this algorithm.
-    // For each predicate that is not IsNotNull, we will generate them one by one loading attributes
-    // as necessary. For each of both attributes, if there is an IsNotNull predicate we will
-    // generate that check *before* the predicate. After all of these predicates, we will generate
-    // the remaining IsNotNull checks that were not part of other predicates.
-    // This has the property of not doing redundant IsNotNull checks and taking better advantage of
-    // short-circuiting, not loading attributes until they are needed.
-    // This is very perf sensitive.
-    // TODO: revisit this. We can consider reordering predicates as well.
-    val generatedIsNotNullChecks = new Array[Boolean](notNullPreds.length)
-    val generated = otherPreds.map { c =>
-      val nullChecks = c.references.map { r =>
-        val idx = notNullPreds.indexWhere { n => n.asInstanceOf[IsNotNull].child.semanticEquals(r)}
-        if (idx != -1 && !generatedIsNotNullChecks(idx)) {
-          generatedIsNotNullChecks(idx) = true
-          // Use the child's output. The nullability is what the child produced.
-          genPredicate(notNullPreds(idx), input, child.output)
-        } else {
-          ""
-        }
-      }.mkString("\n").trim
-
-      // Here we use *this* operator's output with this output's nullability since we already
-      // enforced them with the IsNotNull checks above.
-      s"""
-         |$nullChecks
-         |${genPredicate(c, input, output)}
-       """.stripMargin.trim
-    }.mkString("\n")
-
-    val nullChecks = notNullPreds.zipWithIndex.map { case (c, idx) =>
-      if (!generatedIsNotNullChecks(idx)) {
-        genPredicate(c, input, child.output)
-      } else {
-        ""
-      }
-    }.mkString("\n")
-
-    // Reset the isNull to false for the not-null columns, then the followed operators could
-    // generate better code (remove dead branches).
-    val resultVars = input.zipWithIndex.map { case (ev, i) =>
-      if (notNullAttributes.contains(child.output(i).exprId)) {
-        ev.isNull = "false"
-      }
-      ev
-    }
-
-    println("generated = " + generated)
-    println("nullcheck = " + nullChecks)
-    println("w22222222222222222222222222222222222222")
-    s"""
-       |$generated
-       |$nullChecks
-       |$numOutput.add(1);
-       |${consume(ctx, resultVars)}
-     """.stripMargin
-  }
-
   protected override def doExecute(): RDD[InternalRow] = {
     child.printSchema()
     val numOutputRows = longMetric("numOutputRows")
-    child.execute().mapPartitionsInternal { iter =>
-      println("In execute START")
-      println(child.schema.fieldNames.toList);
-      val predicate = generateJCUDA.generate(condition, child.output)
-      output.map(o => o.dataType)
+    child.execute().mapPartitionsWithIndex { (index, iter) =>
 
-      val result = iter.filter { row =>
-        val r = predicate(row)
-        val idx = schema.fieldIndex("cnt")
-        println(row.get(idx,schema.fields(idx).dataType))
-        if (r) numOutputRows += 1
-        r
+      val buffer = JCUDACodeGen.generateFromFile(child.schema)
+      // val buffer = JCUDACodeGen.generate(child.schema)
+      // val buffer = new JCUDAJava().generateIt(child.schema)
+      buffer.init(index, iter)
+      new Iterator[InternalRow] {
+        override def hasNext: Boolean = buffer.hasNext
+        override def next: InternalRow = buffer.next
       }
-      println("In execute END")
-      result
     }
   }
-
-  override def outputOrdering: Seq[SortOrder] = child.outputOrdering
 }
 
-case class MyFilter(condition: Expression, child: LogicalPlan)
-  extends UnaryNode with PredicateHelper {
+case class MAPGPU(inp: String, child: LogicalPlan)
+  extends UnaryNode {
+
+  // Schema is same
   override def output: Seq[Attribute] = child.output
 
+  //Max output rows for array length
   override def maxRows: Option[Long] = child.maxRows
-
-  override protected def validConstraints: Set[Expression] = {
-    val predicates = splitConjunctivePredicates(condition)
-      .filterNot(SubqueryExpression.hasCorrelatedSubquery)
-    child.constraints.union(predicates.toSet)
-  }
 }
 
 
 
 object GPUOperators extends Strategy {
   def apply(plan: LogicalPlan): Seq[SparkPlan] = plan match {
-    case MyFilter(condition, child) =>
-      println("Mapping Filter -> ExecFilter")
-      FilterExec(condition, planLater(child)) :: Nil
+    case MAPGPU(inp, child) =>
+      println("Mapping MAPGPU -> MAPGPUExec")
+      MAPGPUExec(inp, planLater(child)) :: Nil
     case _ => {
       Nil
     }
@@ -199,11 +69,15 @@ object Utils {
   type _Column = org.apache.spark.sql.Column
 
   implicit class tempClass[T: Encoder](ds: Dataset[T]) {
-    def myFilter(condition: Column): Dataset[T] =  {
-      Dataset(ds.sparkSession, MyFilter(condition.expr, ds.logicalPlan))
+    def mapGPU(inp: String): Dataset[T] =  {
+      Dataset(ds.sparkSession, MAPGPU(inp, ds.logicalPlan))
     }
     ds.sparkSession.experimental.extraStrategies =
       (GPUOperators :: Nil)
+  }
+
+  def getGenericInternalRow(internalRow: InternalRow, schema : StructType) : GenericInternalRow = {
+    new GenericInternalRow(internalRow.toSeq(schema).toArray)
   }
 
 }
