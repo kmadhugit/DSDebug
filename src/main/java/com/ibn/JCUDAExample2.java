018e6809a852a72caeb49f6c287a34c7827f24f3
Madhusudanan Kandasamy <madhusudanan@in.ibm.com>
2016-08-21 20:41:52 +0530

    Buffer example JCUDAExample2

--- src/main/java/com/ibn/JCUDAExample2.java
+++ src/main/java/com/ibn/JCUDAExample2.java
@@ -2,7 +2,12 @@ package com.ibn;
 
 import jcuda.Pointer;
 import jcuda.Sizeof;
+import jcuda.runtime.*;
 import jcuda.driver.*;
+
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import static jcuda.driver.JCudaDriver.*;
 
@@ -12,7 +17,16 @@ import static jcuda.driver.JCudaDriver.*;
 public class JCUDAExample2 {
 
 
+
     public static void main(String args[]) {
+       class myPointer extends CUdeviceptr {
+            myPointer() {
+                super();
+            }
+            public long getBuffer() {
+                return getNativePointer();
+            }
+        }
 
         int numElements = 10;
         long hostInput[] = new long[numElements];
@@ -42,21 +56,28 @@ public class JCUDAExample2 {
 
         // Allocate the device hostinput data, and copy the
         // host hostinput data to the device
-        Pointer ptr = new Pointer();
-        cuMemAllocHost(ptr,80);
+        myPointer myptr = new myPointer();
+        cuMemAllocHost(myptr,80);
+        ByteBuffer buf = myptr.getByteBuffer(0,80).order(ByteOrder.LITTLE_ENDIAN);
         for(int i = 0; i < numElements; i++)
-            ptr.getByteBuffer(0,80).putLong(i*8,i);
+            buf.putLong(i*8,i*10);
 
         // COpy hostinput to Device
         CUdeviceptr deviceinput = new CUdeviceptr();
         cuMemAlloc(deviceinput, numElements * Sizeof.LONG);
-        cuMemcpyHtoD(deviceinput, Pointer.to(ptr),
+        cuMemcpyHtoD(deviceinput,
+                Pointer.to(buf),
                 numElements * Sizeof.LONG);
 
 
-        // Allocate device output memory
         CUdeviceptr deviceoutput = new CUdeviceptr();
         cuMemAlloc(deviceoutput, numElements * Sizeof.LONG);
+
+        System.out.println("host Input buf = " + buf);
+        System.out.println("host Input Pointer.to(buf) = " + Pointer.to(buf));
+        System.out.println("host Input myptr = " + myptr);
+        System.out.println("device Input " + deviceinput);
+        System.out.println("device output" + deviceoutput);
 
         // Set up the kernel parameters: A pointer to an array
         // of pointers which point to the actual values.
@@ -75,19 +96,17 @@ public class JCUDAExample2 {
                 0, null,               // Shared memory size and stream
                 kernelParameters, null // Kernel- and extra parameters
         );
+
         cuCtxSynchronize();
 
         // Allocate host output memory and copy the device output
         // to the host.
-        cuMemcpyDtoH(Pointer.to(hostOutput), deviceoutput,
-                numElements * Sizeof.LONG);
+        cuMemcpyDtoH(Pointer.to(hostOutput), deviceoutput, numElements * Sizeof.LONG);
 
         // Clean up.
+        cuMemFreeHost(myptr);
         cuMemFree(deviceinput);
         cuMemFree(deviceoutput);
-        cuMemFreeHost(ptr);
 
-        for(int i = 0; i < numElements; i++)
-            System.out.println("Input = " + hostInput[i] + "output =" + hostOutput[i]);
     }
 }
--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 rsync -razv --exclude build /Users/madhusudanan/spark-projects/DSDebug/src kmadhu@npk40a.trl.ibm.com:/home/kmadhu/DSDebug
-rsync -razv --exclude build /Users/madhusudanan/spark-projects/DSDebug/pom.xml kmadhu@npk40a.trl.ibm.com:/home/kmadhu/DSDebug
-rsync -razv --exclude build /Users/madhusudanan/spark-projects/DSDebug/bin kmadhu@npk40a.trl.ibm.com:/home/kmadhu/DSDebug
+#rsync -razv --exclude build /Users/madhusudanan/spark-projects/DSDebug/pom.xml kmadhu@npk40a.trl.ibm.com:/home/kmadhu/DSDebug
+#rsync -razv --exclude build /Users/madhusudanan/spark-projects/DSDebug/bin kmadhu@npk40a.trl.ibm.com:/home/kmadhu/DSDebug
